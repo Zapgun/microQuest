9499ec05be573ecd0dd2c8a33f64f0bc30d87997
Zapgun <330793+Zapgun@users.noreply.github.com>
2021-03-28 03:03:17 +0100

    Tried to simplify scaling features. Added custom ui scaler, rejigged orthographic system.

--- Assets/Scripts/MonoBehaviours/PixelPerfectResizer.cs
+++ Assets/Scripts/MonoBehaviours/PixelPerfectResizer.cs
@@ -6,73 +6,22 @@ using UnityEngine.UI;
 /// <summary>
 /// This class will resize the orthographic view of the main camera, virtual cameras, and canvas
 /// to try and keep pixel perfect viewing within the game.
+/// See this post for details: https://blogs.unity3d.com/2015/06/19/pixel-perfect-2d
+/// Formula:  Orthographic size = ((Vert Resolution)/(PPUScale * PPU)) * 0.5
 /// </summary>
 [ExecuteInEditMode]
 public class PixelPerfectResizer : MonoBehaviour {
 
-	float oldScaleFactor;
 	[ReadOnly]
-	public float scaleFactor;
-	public float threshold = 300f;
-	float numPixels;
-	float screenHeight;
-	public float pixelToUnits = 1f;
-	public int gameMultiplier = 1;
-	public static float orthographicSize;
+	public float orthoSize;
 
-	CanvasScaler[] canvasScalers;
-
-	void Awake () {
-
-		SceneManager.sceneLoaded += OnSceneLoaded;
-		Init ();
-
+	public static float orthographicSize {
+		get {
+			float sf = Mathf.Max(Mathf.Ceil (Mathf.Pow (Screen.height, 0.5f) / 16f),1f);
+			return (Screen.height / (sf * 16f)) * 0.5f;}
 	}
 
-	void OnSceneLoaded (Scene scene, LoadSceneMode mode) {
-		Debug.Log ("Scene '" + scene.name + "' loaded.");
-		Init ();
-	}
-
-	void Init () {
-		canvasScalers = GameObject.FindObjectsOfType<CanvasScaler> ();
-		screenHeight = Screen.height;
-		SetOrthographicSize ();
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-		float tmp = Screen.height * Screen.width;
-		if (numPixels != tmp) {
-			numPixels = tmp;
-			//scaleFactor = Mathf.Max(Mathf.Ceil (Mathf.Pow (numPixels, 0.5f) / threshold),1f);
-			scaleFactor = Mathf.Max (Mathf.Pow (numPixels, 0.5f) / threshold);
-		}
-
-		if (oldScaleFactor != scaleFactor) {
-			foreach (CanvasScaler cs in canvasScalers) {
-				cs.scaleFactor = scaleFactor;
-			}
-		}
-		if (Screen.height != screenHeight || oldScaleFactor != scaleFactor) {
-			screenHeight = Screen.height;
-			SetOrthographicSize ();
-		}
-
-		oldScaleFactor = scaleFactor;
-	}
-
-	void SetOrthographicSize () {
-
-		orthographicSize = (screenHeight / 2.0f) / (pixelToUnits * scaleFactor * gameMultiplier);
-		var camera = Camera.main;
-
-		if (camera != null) {
-			Cinemachine.CinemachineBrain brain = camera.GetComponent<Cinemachine.CinemachineBrain> ();
-			if (camera.orthographic && brain == null) { // Disable local call if using Cinemachine
-				camera.orthographicSize = orthographicSize;
-			}
-		}
+	void Update() {
+		orthoSize = orthographicSize;
 	}
 }
